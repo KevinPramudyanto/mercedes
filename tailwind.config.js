413178f031f42b1fc0435faeb69a8f30153397ab
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2025-02-27 18:42:15 +0800

    Create boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        lightyellow: "#E1A520",
+        darkyellow: "#C18500",
+        darkbrown: "#5F4112",
+        lightbrown: "#695E3E",
+        lightgray: "#656F80",
+        darkgray: "262A31",
+      },
+    },
+  },
+  plugins: [],
+};
