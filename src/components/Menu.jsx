db2241aa6ede66f0274fbd2eefb90a580e88c0f4
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2025-03-02 13:25:35 +0800

    Update tablet mobile css for menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,77 +6,83 @@ const Menu = () => {
   const handleClick = () => setIsMenuOpen((prev) => !prev);
 
   return (
-    <div className="fixed right-7 top-0 z-40 flex flex-col items-center justify-center gap-1">
+    <div className="fixed right-0 top-0 z-40 flex flex-row-reverse items-center justify-center gap-1 opacity-80 sm:right-7 sm:flex-col">
       <div className="flex h-[66px] w-[66px] items-center justify-center bg-lightgray">
         <img src="nav-hamburger.svg" alt="Nav Hamburger" loading="lazy" />
       </div>
 
-      <nav className="flex min-h-[66px] w-[66px] items-center justify-center rounded-b-full bg-lightgray">
+      <nav className="flex min-h-[66px] min-w-[66px] items-center justify-center rounded-l-full bg-lightgray sm:rounded-b-full sm:rounded-tl-none">
         {isMenuOpen ? (
-          <div className="flex flex-col items-center justify-center gap-6 p-3">
-            <a
-              href="https://www.whatsapp.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="invert"
-                src="nav-whatsapp.svg"
-                alt="Nav Whatsapp"
-                loading="lazy"
-              />
-            </a>
+          <div className="flex flex-row-reverse items-center justify-center sm:flex-col">
+            <div className="flex flex-row items-center justify-center gap-2 p-3 sm:flex-col sm:gap-6">
+              <a
+                href="https://www.whatsapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="invert"
+                  src="nav-whatsapp.svg"
+                  alt="Nav Whatsapp"
+                  loading="lazy"
+                />
+              </a>
 
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="invert"
-                src="nav-instagram.svg"
-                alt="Nav Instagram"
-                loading="lazy"
-              />
-            </a>
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="invert"
+                  src="nav-instagram.svg"
+                  alt="Nav Instagram"
+                  loading="lazy"
+                />
+              </a>
 
-            <a
-              href="https://web.telegram.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="invert"
-                src="nav-telegram.svg"
-                alt="Nav Telegram"
-                loading="lazy"
-              />
-            </a>
+              <a
+                href="https://web.telegram.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="invert"
+                  src="nav-telegram.svg"
+                  alt="Nav Telegram"
+                  loading="lazy"
+                />
+              </a>
 
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                className="invert"
-                src="nav-facebook.svg"
-                alt="Nav Facebook"
-                loading="lazy"
-              />
-            </a>
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="invert"
+                  src="nav-facebook.svg"
+                  alt="Nav Facebook"
+                  loading="lazy"
+                />
+              </a>
 
-            <a href="https://x.com/" target="_blank" rel="noopener noreferrer">
-              <img
-                className="invert"
-                src="nav-twitter.svg"
-                alt="Nav Twitter"
-                loading="lazy"
-              />
-            </a>
+              <a
+                href="https://x.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="invert"
+                  src="nav-twitter.svg"
+                  alt="Nav Twitter"
+                  loading="lazy"
+                />
+              </a>
+            </div>
 
             <img
-              className="hover:cursor-pointer"
+              className="rotate-90 p-3 hover:cursor-pointer sm:rotate-0"
               src="nav-arrow-up.svg"
               alt="Nav Arrow Up"
               loading="lazy"
